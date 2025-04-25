d221e0346cd947db3625383bc5d40275868a8af1
Daniel Tanner <danielsptanner@gmail.com>
2025-04-25 11:41:09 +0100

    Added and commented out browser exeuctable location setting for use with wsl- using default from windows is more stable it seems.

--- web_scraper/papeScrape.py
+++ web_scraper/papeScrape.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from pathlib import Path
 from selenium.webdriver.common.by  import By
 from selenium.webdriver.support.ui import Select
@@ -13,8 +13,8 @@ def find_papers(driver):
     papers=[]
     for fname, row in zip(file_names, rows):
         paper_desc   = row.find_element(By.XPATH, "./td[1]").text
-        link_element = row.find_element(By.XPATH, "./td[2]/a")  
-        paperLink = link_element.get_attribute("href")  
+        link_element = row.find_element(By.XPATH, "./td[2]/a")
+        paperLink = link_element.get_attribute("href")
 
 
         papers.append((fname, paper_desc,paperLink) )
@@ -33,52 +33,55 @@ if __name__=="__main__":
     year1 = sys.argv[1] if sys.argv[1] < sys.argv[2] else sys.argv[2]
     year2 = sys.argv[2] if sys.argv[2] > sys.argv[1] else sys.argv[1]
     subject = sys.argv[3]
-    
+
     print(f"Downloading papers from {year1} to {year2}")
-    
+
     url      = "https://www.examinations.ie"
     exam     = "Leaving Certificate"
     dl_dir   = Path.home() / 'Downloads'
     exam_dir = Path(__file__).parent.parent / "Exams"
-    
+    #browse_exe = Path(os.environ["BROWSER"])
+
+
     options = uc.ChromeOptions()
     options.add_experimental_option("prefs", {
-        "download.default_directory": str(dl_dir),  
-        "download.prompt_for_download": False,  
-        "plugins.always_open_pdf_externally": True,  
+        "download.default_directory": str(dl_dir),
+        "download.prompt_for_download": False,
+        "plugins.always_open_pdf_externally": True,
     })
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
+    #options.binary_location = str(browse_exe)
     driver = uc.Chrome(options=options)
-    
+
     print(f"Get {url}")
     driver.get(url)                                                                 ; sleep(13)
     driver.get(url+"/exammaterialarchive")                                          ; sleep(5)
     driver.find_element(By.ID, "MaterialArchive__noTable__cbv__AgreeCheck").click() ; sleep(5)
-    
+
     for year in range(int(year1), int(year2)+1):
-        dropDowns = [("MaterialArchive__noTable__sbv__ViewType","Exam Papers" ), 
-                     ("MaterialArchive__noTable__sbv__YearSelect",str(year) ), 
+        dropDowns = [("MaterialArchive__noTable__sbv__ViewType","Exam Papers" ),
+                     ("MaterialArchive__noTable__sbv__YearSelect",str(year) ),
                      ("MaterialArchive__noTable__sbv__ExaminationSelect",exam),
                      ("MaterialArchive__noTable__sbv__SubjectSelect", subject )
                     ]
-    
+
         # Do clicking and drop down selection
         for menu, selection in dropDowns:
             print(f"{menu}: {selection}")
             dropdown = driver.find_element(By.ID, menu)
             select   = Select(dropdown)
             select.select_by_visible_text(selection) ; sleep(3)
-        
+
         papers= find_papers(driver)
         # Download all papers of all levels for this subject for this year. Organise and log.
         for fname, paperDesc, paperLink in papers:
             # Let's just ignore orals for now
             if ".mp3" in fname:
                 continue
-                
-            level = fname[5:7] 
+
+            level = fname[5:7]
             levels      = { "AL":"Higher", "GL":"Ordinary", "CL":"Common","BL":"Foundation", "ZL":"Sound file"}
             dest        = exam_dir / subject.lower().replace(' ','_') / level
             file_dest   = dest / f"{fname.rstrip('.pdf')}_{year}.pdf"
@@ -101,7 +104,6 @@ if __name__=="__main__":
             elif file_source.exists() and not file_dest.exists():
                 file_source.rename(file_dest)
 
-    
+
     driver.close()
     sleep(4)
-
