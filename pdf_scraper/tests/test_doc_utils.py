78788cf6d2488b9fb37787ff336040bc6f227743
Daniel Tanner <danielsptanner@gmail.com>
2025-06-08 14:15:01 +0100

    various tests on captions and image extractions included

--- pdf_scraper/tests/test_doc_utils.py
+++ pdf_scraper/tests/test_doc_utils.py
@@ -1,4 +1,5 @@
-from pdf_scraper.doc_utils import open_exam, get_images
+from pdf_scraper.doc_utils import open_exam, get_images, get_doc_line_df
+from pdf_scraper.doc_utils import get_captions
 from fitz import Document
 
 
@@ -13,19 +14,39 @@ def test_open_doc():
 
     assert isinstance(doc, Document)
 
-def check_year(year, expected_images):
-    doc = open_exam(year, "English", "AL", 1)
-    images = get_images(doc)
-    assert len(images)== expected_images
 
 def test_get_images():
+    def check_year(year, expected_images):
+        doc = open_exam(year, "English", "AL", 1)
+        images = get_images(doc)
+        assert len(images)== expected_images
     check_year(2025,6)
     check_year(2024,8)
     check_year(2023,17)
     check_year(2003,4)
 
 def test_point_filter():
-    check_year(2006,10)
+    doc = open_exam(2006, "English", "AL", 1)
+    images = get_images(doc)
+    assert len(images)== 10
+
+def test_get_captions():
+    def check_nth_caption(year, expected_caption,n):
+        doc = open_exam(year, "English", "AL", 1)
+        doc_df = get_doc_line_df(doc)
+        images = get_images(doc)
+        images = get_captions(doc_df, images)
+        captioned_images = [img for img in images if img["caption"]]
+        if captioned_images:
+            caption = captioned_images[n]["caption"]
+        else:
+            caption=''
+        assert caption == expected_caption
+    check_nth_caption(2025, 'Underdog Jamaican Bobsled Team', 0)
+    check_nth_caption(2024, '', 0)
+    check_nth_caption(2023,'Abdulrazak  Gurnah',0)
+    check_nth_caption(2020, '', 0)
+
 
 
 
