f6861dec95862f57c077e8419868cc62b65f036d
Daniel Tanner <danielsptanner@gmail.com>
2025-06-08 14:13:55 +0100

    caption extracting functions

--- pdf_scraper/doc_utils.py
+++ pdf_scraper/doc_utils.py
@@ -53,6 +53,12 @@ def get_doc_line_df(doc):
 
     return doc_df
 
+def filter_point_images(images):
+    def is_point_image(img, threshold=5):
+        x0, y0, x1, y1 = img["bbox"]
+        return (x1 - x0) < threshold and (y1 - y0) < threshold
+    return [img for img in images if not is_point_image(img) ]
+
 def get_images(doc):
     images = []
     for i, page in enumerate(doc):
@@ -61,17 +67,55 @@ def get_images(doc):
         image_blocks = [block for block in page_blocks if     block["type"]]
         for image_block in image_blocks:
             image_block["page"]= i+1
+            image_block["caption"] = ""
 
         images.extend(image_blocks)
 
     if len(images) > 500:
-        images=filter_images(images)
+        images=filter_point_images(images)
 
     return images
 
-def filter_images(images):
-    def is_point_image(img, threshold=5):
-        x0, y0, x1, y1 = img["bbox"]
-        return (x1 - x0) < threshold and (y1 - y0) < threshold
-    #return [img for img in images if img["size"] > 2000 ]
-    return [img for img in images if not is_point_image(img) ]
+
+def get_in_image_captions(doc_df: pd.DataFrame, images: list[dict]) -> list[dict]:
+    """
+    Add captions to images based on overlapping boxes. For english paper one, we
+    will not caption anything on the first page, or after the 8th
+    """
+    for image in images:
+        if image["page"] == 1 or image["page"] >8:
+            continue
+        img_rect = fitz.Rect(*image["bbox"])
+
+        # Filter all potentially overlapping rows using bounding box logic
+        overlap_mask = (
+            (doc_df["x1"] > img_rect.x0 + 0.2) &
+            (doc_df["x0"] < img_rect.x1) &
+            (doc_df["y1"] > img_rect.y0 + 0.2) &
+            (doc_df["y0"] < img_rect.y1) &
+            (doc_df["page"] == image["page"] )
+        )
+        overlapping_rows = doc_df[overlap_mask]
+
+        if len(overlapping_rows) > 0 and image["page"]!=1 :
+            print(overlapping_rows[["text","page"]].head(4))
+
+        overlapping_rows = overlapping_rows.sort_values(by="y0")
+
+        # Collect and join the text
+        image["caption"] = " ".join(overlapping_rows["text"].astype(str)).strip()
+
+    return images
+
+def get_captions(doc_df: pd.DataFrame, images: list[dict]) -> list[dict]:
+    """
+    Add captions to images based on overlapping boxes. For english paper one, we
+    will not caption anything on the first page, or after the 8th
+    """
+    images = get_in_image_captions(doc_df, images)
+    # Captions to add manually
+    # 2019 p6: 'Warstones\xa0Library\xa0'
+    # 2014 p3: 'Canada by Richard Ford – book cover '
+    # 2013
+
+    return images
