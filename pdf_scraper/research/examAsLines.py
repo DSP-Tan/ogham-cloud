6db9b935bd57fbeb9189900b18170077bb601361
Daniel Tanner <danielsptanner@gmail.com>
2025-06-08 14:15:32 +0100

    image stitching function first version finished.

--- pdf_scraper/research/examAsLines.py
+++ pdf_scraper/research/examAsLines.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from pdf_scraper.doc_utils import open_exam
+from pdf_scraper.doc_utils import open_exam, get_doc_line_df
 from pdf_scraper.block_utils import clean_blocks
 from pdf_scraper.line_utils import print_line_table, get_line_df
 
@@ -8,25 +8,6 @@ from pdf_scraper.line_utils import print_line_table, get_line_df
 # 2. Get all text lines.
 # 3. Identify and Remove captions from text lines.
 # 4. Identify and resort dual column text.
-
-def get_doc_line_df(doc):
-    dfs = []
-    for i, page in enumerate(doc):
-        page_blocks  = page.get_text("dict",sort=True)["blocks"]
-
-        text_blocks  = [block for block in page_blocks if not block["type"]]
-        text_blocks = clean_blocks(text_blocks)
-
-        page_lines   = [ line for block in text_blocks for line in block["lines"]]
-        page_df = get_line_df(page_lines)
-        page_df["page"] = i+1
-        page_df.sort_values("y0",inplace=True)
-        dfs.append(page_df)
-    doc_df = pd.concat(dfs,ignore_index=True)
-    doc_df["dual_col"]=0
-
-    return doc_df
-
 
 def setDualCols(doc_df: pd.DataFrame, page_num:int, bookends: tuple[str]):
     page_df = doc_df[doc_df.page==page_num].copy()
@@ -54,48 +35,49 @@ def setDualCols(doc_df: pd.DataFrame, page_num:int, bookends: tuple[str]):
 
     return doc_df
 
-year=2020
+year=2001
 doc = open_exam(year,"english","al",1)
+
+#import ipdb; ipdb.set_trace()
 doc_df = get_doc_line_df(doc)
 
-import ipdb; ipdb.set_trace()
 print(doc_df["text"].iloc[20:50].head(30))
 
-# page 2
-line_l1 = "I flit anxious"
-line_r1 = "yourself be led by the child"
-line_lf = "imagination had not abandoned me"
-line_rf = "lose almost all the time"
-bookends = [line_l1,line_r1, line_lf, line_rf]
-
-doc_df = setDualCols(doc_df, 2, bookends)
-
-print(doc_df["text"].iloc[20:50].head(30))
-
-# page 4
-line_l1 = "I had called upon my friend"
-line_r1 = "Peterson was left in possesion"
-line_lf = "streets. The roughs had also fled"
-line_rf = "to be deduced from his hat."
-bookends = [line_l1,line_r1, line_lf, line_rf]
-
-doc_df = setDualCols(doc_df, 4, bookends)
-
-# page 5
-line_l1 = "'You are certainly joking, Holmes"
-line_r1 = "ago, and has had no hat since, then he has"
-line_lf = "could afford to buy so expensive a hat three years"
-line_rf = "cheeks, dazed with astonishment"
-bookends = [line_l1,line_r1, line_lf, line_rf]
-
-doc_df = setDualCols(doc_df, 4, bookends)
-
-
-# page 6
-line_l1 = "My name is Ariadne"
-line_r1 = "Inflatable habitat modules"
-line_lf = "have as minimal an impact"
-line_rf = "rewards: Quiet. Beauty. Understanding"
-bookends = [line_l1,line_r1, line_lf, line_rf]
-
-doc_df = setDualCols(doc_df, 4, bookends)
+## page 2
+#line_l1 = "I flit anxious"
+#line_r1 = "yourself be led by the child"
+#line_lf = "imagination had not abandoned me"
+#line_rf = "lose almost all the time"
+#bookends = [line_l1,line_r1, line_lf, line_rf]
+#
+#doc_df = setDualCols(doc_df, 2, bookends)
+#
+#print(doc_df["text"].iloc[20:50].head(30))
+#
+## page 4
+#line_l1 = "I had called upon my friend"
+#line_r1 = "Peterson was left in possesion"
+#line_lf = "streets. The roughs had also fled"
+#line_rf = "to be deduced from his hat."
+#bookends = [line_l1,line_r1, line_lf, line_rf]
+#
+#doc_df = setDualCols(doc_df, 4, bookends)
+#
+## page 5
+#line_l1 = "'You are certainly joking, Holmes"
+#line_r1 = "ago, and has had no hat since, then he has"
+#line_lf = "could afford to buy so expensive a hat three years"
+#line_rf = "cheeks, dazed with astonishment"
+#bookends = [line_l1,line_r1, line_lf, line_rf]
+#
+#doc_df = setDualCols(doc_df, 4, bookends)
+#
+#
+## page 6
+#line_l1 = "My name is Ariadne"
+#line_r1 = "Inflatable habitat modules"
+#line_lf = "have as minimal an impact"
+#line_rf = "rewards: Quiet. Beauty. Understanding"
+#bookends = [line_l1,line_r1, line_lf, line_rf]
+#
+#doc_df = setDualCols(doc_df, 4, bookends)
