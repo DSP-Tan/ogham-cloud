ee680c2ba4a74d35a9c1400550837946640ccdd1
Daniel Tanner <danielsptanner@gmail.com>
2025-06-10 17:39:20 +0100

    tests for new image_utils module added

---
+++ pdf_scraper/tests/test_image_utils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from pdf_scraper.doc_utils   import open_exam, get_doc_line_df, get_images
+from pdf_scraper.image_utils import get_in_image_captions, get_in_image_lines
+
+
+def test_get_in_image_lines():
+    def get_image_and_doc(year:int, page:int, n_image:int, expected_index:pd.Index):
+        doc = open_exam(year, "English", "AL", 1)
+        doc_df = get_doc_line_df(doc)
+        images = get_images(doc)
+        image = [img for img in images if img["page"]==page][n_image]
+        index = get_in_image_lines(doc_df, image)
+        assert all(index == expected_index)
+    get_image_and_doc(2023,3,0,pd.Index([144,145]))
+    get_image_and_doc(2025,2,0,pd.Index([43]))
+
+
+
+
+
+
+def test_get_in_image_captions():
+    def get_image_and_doc(year, page, n_image, expected_caption):
+        doc = open_exam(year, "English", "AL", 1)
+        doc_df = get_doc_line_df(doc)
+        images = get_images(doc)
+        image = [img for img in images if img["page"]==page][n_image]
+        caption = get_in_image_captions(doc_df, image)
+        assert caption == expected_caption
+    get_image_and_doc(2023,3,0,'Abdulrazak \nGurnah')
+    get_image_and_doc(2025,2,0,'Underdog Jamaican Bobsled Team')
