ca5006c72e9f6b517c9459002bb7b32a23369ee0
Daniel Tanner <danielsptanner@gmail.com>
2025-06-14 17:34:04 +0100

    save to temporary development testing scripts

--- pdf_scraper/research/check.py
+++ pdf_scraper/research/check.py
@@ -6,22 +6,60 @@ import numpy as np
 
 from pdf_scraper.doc_utils import open_exam, get_images, filter_point_images
 from pdf_scraper.doc_utils import get_doc_line_df
-from pdf_scraper.doc_utils import get_captions
+from pdf_scraper.doc_utils import get_captions, filter_images, assign_in_image_captions
 from pdf_scraper.block_utils import clean_blocks
 from pdf_scraper.line_utils import print_line_table, get_line_df
 from pdf_scraper.image_utils import is_point_image, is_horizontal_strip,filter_point_images, filter_horizontal_strips
 from pdf_scraper.image_utils import filter_horizontal_strips,get_stripped_images,stitch_strips, reconstitute_strips
+from pdf_scraper.line_utils import closest_vertical_line
+from pdf_scraper.general_utils import bbox_horiz_dist, shared_centre
 
-year=2013
+def closest_line_closest_thing(image,doc_df):
+    n_page   = image["page"]
+    img_bbox = image["bbox"]
+    page_df = doc_df[doc_df.page==image["page"]].copy()
+    # I think just centred would be better to have here. There can be a caption which
+    # wider than the image.
+    overlap = page_df.apply(
+        lambda row: bbox_horiz_dist((row["x0"],row["y0"],row["x1"],row["y1"] ),image["bbox"])==0,
+        axis=1
+        )
+    overlap_df = page_df[overlap]
+    centred = page_df.apply(
+        lambda row: shared_centre( (row["x0"],row["y0"],row["x1"],row["y1"] ),image["bbox"]) ,
+        axis=1
+    )
+    centred_df = page_df[overlap & centred]
+    if len(centred_df) ==0:
+        return None
+    idx, dist = closest_vertical_line(img_bbox, centred_df, n_page)
+    line_bbox = tuple(centred_df.loc[idx][["x0","y0","x1","y1"]])
+
+    if (len(centred_df)==1):
+        if dist > 30:
+            return None
+        else:
+            return idx
+
+    idx1, dist1 = closest_vertical_line(line_bbox, overlap_df, n_page)
+    line1_bbox  = tuple(overlap_df.loc[idx1][["x0","y0","x1","y1"]])
+
+    if dist < dist1:
+        print("The line which is closest to this image is closer to the image than it's nearest line.")
+        return idx
+    return None
+
+year=2025
 doc = open_exam(year,"english","al",1)
 doc_df = get_doc_line_df(doc)
 images = get_images(doc)
-
 print(f"number of raw images                : {len(images):10}")
-images = filter_point_images(images)
-print(f"number of images after  point filter: {len(images):10}")
-strips = get_stripped_images(images)
-print(f"number of stripped images: {len(stripped):10}")
-import ipdb; ipdb.set_trace()
+images = filter_images(images)
+print(f"number of filtered/fixed images     : {len(images):10}")
 
-images = get_captions(doc_df, images)
+
+caption_indices, n_img = assign_in_image_captions(doc_df, images)
+
+
+import ipdb; ipdb.set_trace()
+#closest_line_closest_thing(images[2],doc_df)
--- pdf_scraper/research/quickTest.py
+++ pdf_scraper/research/quickTest.py
@@ -6,7 +6,7 @@ from fitz import Rect
 import sys
 
 from pdf_scraper.block_utils import identify_dual_column, get_block_text, sort_dual_column_blocks
-from pdf_scraper.doc_utils import open_exam
+from pdf_scraper.doc_utils   import open_exam, get_doc_line_df
 from pdf_scraper.block_utils import is_empty_block, clean_blocks, print_block_table, get_block_table, rebox_blocks
 from pdf_scraper.block_utils import preproc_blocks
 from pdf_scraper.draw_utils  import get_pink_boundary, get_fill_df, in_the_pink
@@ -16,26 +16,36 @@ from pdf_scraper.line_utils  import get_line_df, print_line_table, get_all_lines
 pd.set_option("display.float_format", "{:.2f}".format)
 
 
+#new_doc = fitz.open()
+#for year in range(2001,2026):
+#    doc = open_exam(year, "english", "al",1)
+#    for p in [3, 5, 7]:
+#        new_doc.insert_pdf(doc, from_page=p-1, to_page=p-1)
+#    doc.close()
+#
+#new_doc.save("check_NBS.pdf")
+#new_doc.close()
 
-year = int(sys.argv[1])
+doc = open_exam(2017)
+df = get_doc_line_df(doc)
+# The n.b instruction line is usually:
+# "N.B. Candidates may NOT answer Question A and Question B on the same text."
+# However in 2021 it says
+# "N.B. Answer only ONE question in Section I, either one Question A OR one Question B on one text."
 
-doc = open_exam(year, "english","al",1 )
 
-def has_empty_line(block):
-    if block["type"]:
-        return False
-    for line in block["lines"]:
-        if line_is_empty(line):
-            return True
-    return False
+for year in range(2001,2026):
+    doc = open_exam(year, "english", "al",1)
+    df = get_doc_line_df(doc)
+    page = (df.page != 1)
+    pattern1 = df.text.str.lstrip().str.contains(r"^N\.B\.")
+    pattern2 = df.text.str.contains(r"^Candidates may NOT")
+    pattern3 = df.text.str.contains(r"^Questions.*A.*and.*B.*carry.*50.*marks.*each")
 
-for i, page in enumerate(doc):
-    page_dict  = page.get_text("dict",sort=True)
-    blocks    = page_dict["blocks"]
-    non_empty_blocks = [block for block in blocks if not is_empty_block(block)]
+    general_nb_pattern = r"N\.B\..*[aA]nswer.*Question.*A.*Question.*B.*on.*text"
+    instructs = df[page & (pattern1 | pattern2 | pattern3) ]
+    print(f"{'year':<5}:{'count':<5}")
+    print(f"{year:<5}:{len(instructs):<5}")
+    print(instructs.head(10))
 
-    for n_block, block in enumerate(non_empty_blocks):
-        if has_empty_line(block):
-            print(f"page: {i}, block: {n_block}, year {year}")
-
-import ipdb; ipdb.set_trace()
+    doc.close()
