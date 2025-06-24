5887ec4d04ec542f8a2daaf4104c9b0637ea77a4
Daniel Tanner <danielsptanner@gmail.com>
2025-06-24 21:23:34 +0100

    footer extraction

--- pdf_scraper/research/quickTest.py
+++ pdf_scraper/research/quickTest.py
@@ -16,36 +16,29 @@ from pdf_scraper.line_utils  import get_line_df, print_line_table, get_all_lines
 pd.set_option("display.float_format", "{:.2f}".format)
 
 
-#new_doc = fitz.open()
-#for year in range(2001,2026):
-#    doc = open_exam(year, "english", "al",1)
-#    for p in [3, 5, 7]:
-#        new_doc.insert_pdf(doc, from_page=p-1, to_page=p-1)
-#    doc.close()
-#
-#new_doc.save("check_NBS.pdf")
-#new_doc.close()
-
-doc = open_exam(2017)
-df = get_doc_line_df(doc)
-# The n.b instruction line is usually:
-# "N.B. Candidates may NOT answer Question A and Question B on the same text."
-# However in 2021 it says
-# "N.B. Answer only ONE question in Section I, either one Question A OR one Question B on one text."
-
-
 for year in range(2001,2026):
     doc = open_exam(year, "english", "al",1)
-    df = get_doc_line_df(doc)
-    page = (df.page != 1)
-    pattern1 = df.text.str.lstrip().str.contains(r"^N\.B\.")
-    pattern2 = df.text.str.contains(r"^Candidates may NOT")
-    pattern3 = df.text.str.contains(r"^Questions.*A.*and.*B.*carry.*50.*marks.*each")
+    df = get_doc_line_df(doc).drop(columns=["caption","dual_col","instruction"])
 
-    general_nb_pattern = r"N\.B\..*[aA]nswer.*Question.*A.*Question.*B.*on.*text"
-    instructs = df[page & (pattern1 | pattern2 | pattern3) ]
-    print(f"{'year':<5}:{'count':<5}")
-    print(f"{year:<5}:{len(instructs):<5}")
-    print(instructs.head(10))
+    df['rank'] = df.groupby('page')['y0'].rank(method='first', ascending=False)
+
+    n_page_regex= r'page[ \xa0](?:1[0-2]|[1-9])[ \xa0]of[ \xa0](?:1[0-2]|[1-9])'
+    lc_regex    = r'leaving[ \xa0]certificate[ \xa0]examination[ \xa0]20[0-9][0-9]'
+    eng_regex   = r'english[ \xa0]–[ \xa0]higher[ \xa0]level[ \xa0]–[ \xa0]paper[ \xa0]1'
+    num_regex   = r'^(?:1[0-2]|[1-9])$'
+
+    pattern1 = df.text.str.lower().str.contains(n_page_regex, regex=True)
+    pattern2 = df.text.str.lower().str.contains(lc_regex, regex=True)
+    pattern3 = df.text.str.lower().str.contains(eng_regex, regex=True)
+    pattern4 = df.text.str.strip().str.contains(num_regex, regex=True)
+    bottom = df['rank'] <= 3
+
+    result = df[ bottom & (pattern1 | pattern2 | pattern3 | pattern4 ) ].drop(columns=["rank"],inplace=False)
+
+
+    print(year)
+    print("--"*40)
+    print(result.head(20))
+    print("--"*40)
 
     doc.close()
