1f84cb4785cdf05c61c3851b938ce869b1b9c37d
Daniel Tanner <danielsptanner@gmail.com>
2025-07-01 16:39:36 +0100

    testing heading identifier

--- pdf_scraper/research/quickTest.py
+++ pdf_scraper/research/quickTest.py
@@ -6,7 +6,7 @@ from fitz import Rect
 import sys, re
 
 from pdf_scraper.block_utils import identify_dual_column, get_block_text, sort_dual_column_blocks
-from pdf_scraper.doc_utils   import open_exam, get_doc_line_df
+from pdf_scraper.doc_utils   import open_exam, get_doc_line_df, identify_section_headers
 from pdf_scraper.block_utils import is_empty_block, clean_blocks, print_block_table, get_block_table, rebox_blocks
 from pdf_scraper.block_utils import preproc_blocks
 from pdf_scraper.draw_utils  import get_pink_boundary, get_fill_df, in_the_pink
@@ -26,26 +26,32 @@ Section Titles will have the following properties:
 
 for year in range(2001,2026):
     doc = open_exam(year, "english", "al",1)
-    df = get_doc_line_df(doc).drop(columns=["caption","dual_col","instruction"])
+    df = get_doc_line_df(doc)
 
-
+    identify_section_headers(df)
     df['rank'] = df.groupby('page')['y0'].rank(method='first', ascending=True)
 
-    section_regex= r'section[\xa0 ]*(?:I{1,2}|[1-2])'
-    compre_regex = r'comprehending'
-    compos_regex = r'composing'
-    marks_regex  = r'\(\d{1,3}\)\s*marks'
+    if df.section.sum() == 0:
+        raise RuntimeError("Assign section headings first")
 
-    pattern1 = df.text.str.contains(section_regex,flags=re.IGNORECASE, regex=True)
-    pattern2 = df.text.str.lower().str.contains(compre_regex, regex=True)
-    pattern3 = df.text.str.lower().str.contains(compos_regex, regex=True)
-    pattern4 = df.text.str.strip().str.contains(marks_regex, regex=True)
-    top = df['rank'] <= 4
+    middle        = doc[0].rect.width/2
+    median_font   = df.font_size.median()
+    large_font    = df.font_size >= median_font*1.15
+    bold_font     = df.mode_font.str.contains("Bold")
+    pages        =  (df.page > 1) & (df.page <9)
+    centred       = ( (df.x0 + df.x1)/2 > middle -30 ) & ( (df.x0 + df.x1)/2 < middle +30 )
+    uncategorised = (df.section==0) & (df.caption ==0) & (df.instruction==0)
+    top           = df['rank'] <= 20
 
-    result = df[ top & (pattern1 | pattern2 | pattern3 | pattern4 ) ].drop(columns=["rank"],inplace=False)
+    result = df.loc[large_font & bold_font & pages & uncategorised & centred & top , ["text","font_size","font_sizes", "page","mode_font","common_font"]].copy()
+    result["font_multip"] = result.font_size/median_font
+
+    if year==2006:
+        import ipdb; ipdb.set_trace()
 
 
     print(year)
+    print(f"median font size: {median_font}")
     print("--"*40)
     print(len(result))
     print(result.head(40))
