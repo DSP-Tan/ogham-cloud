61eb702437c73c2d4d84b7c3fb80a0196d2e6ec9
Daniel Tanner <danielsptanner@gmail.com>
2025-08-21 15:25:34 +0200

    fixed section heading identification

--- pdf_scraper/doc_utils.py
+++ pdf_scraper/doc_utils.py
@@ -179,12 +179,12 @@ def identify_section_headers(doc_df):
     section_regex= r'section[\xa0 ]*(?:I{1,2}|[1-2])'
     compre_regex = r'comprehending'
     compos_regex = r'composing'
-    marks_regex  = r'\(\d{1,3}\)\s*marks'
+    marks_regex  = r'\(\d{1,3}\s*marks\)'
 
     pattern1 = doc_df.text.str.contains(section_regex,flags=re.IGNORECASE, regex=True)
     pattern2 = doc_df.text.str.lower().str.contains(compre_regex, regex=True)
     pattern3 = doc_df.text.str.lower().str.contains(compos_regex, regex=True)
-    pattern4 = doc_df.text.str.strip().str.contains(marks_regex, regex=True)
+    pattern4 = doc_df.text.str.lower().str.strip().str.contains(marks_regex, regex=True)
     top = doc_df['rank'] <= 4
 
     mask = top & (pattern1 | pattern2 | pattern3 | pattern4 )
