9a42eeb8066fc8ac9bf605518ed41c3a8a688cfb
Daniel Tanner <danielsptanner@gmail.com>
2025-08-23 17:36:23 +0200

    subtitle identify development

--- pdf_scraper/research/quickTest.py
+++ pdf_scraper/research/quickTest.py
@@ -7,7 +7,7 @@ import sys, re
 
 from pdf_scraper.block_utils import identify_dual_column, get_block_text, sort_dual_column_blocks
 from pdf_scraper.doc_utils   import open_exam, get_doc_line_df, identify_section_headers, identify_text_headers
-from pdf_scraper.doc_utils   import identify_footers, identify_instructions
+from pdf_scraper.doc_utils   import identify_footers, identify_instructions, identify_subtitles
 from pdf_scraper.block_utils import is_empty_block, clean_blocks, print_block_table, get_block_table, rebox_blocks
 from pdf_scraper.block_utils import preproc_blocks
 from pdf_scraper.draw_utils  import get_pink_boundary, get_fill_df, in_the_pink
@@ -16,7 +16,7 @@ from pdf_scraper.line_utils  import get_line_df, print_line_table, get_all_lines
 pd.set_option("display.float_format", "{:.2f}".format)
 
 """
-subtltles
+subtitles
 - bold
 - at top of page under titles
 - only on pages where there are titles.
@@ -39,20 +39,22 @@ for year in range(2001,2026):
     identify_instructions(df)
     identify_section_headers(df)
     identify_text_headers(df, doc_width)
+    identify_subtitles(df)
     
-    uncategorised = (df.section==0) & (df.caption ==0) & (df.instruction==0) & (df.title ==0 ) & (df.footer==0)
-    after_headers = df[uncategorised].groupby('page')['y0'].rank(method='first', ascending=True) <=6
-    title_on_page = df.groupby('page')['title'].transform('sum') >0
-    bold_font     = df.mode_font.str.contains("Bold")
-    pages         =  (df.page > 1) & (df.page <9)
+    #uncategorised = (df.section==0) & (df.caption ==0) & (df.instruction==0) & (df.title ==0 ) & (df.footer==0)
+    #after_headers = df[uncategorised].groupby('page')['y0'].rank(method='first', ascending=True) <=6
+    #title_on_page = df.groupby('page')['title'].transform('sum') >0
+    #bold_font     = df.mode_font.str.contains("Bold")
+    #pages         =  (df.page > 1) & (df.page <9)
     
-    #large_font    = df.font_size >= median_font*1.15
-    #centred       = ( (df.x0 + df.x1)/2 > middle -30 ) & ( (df.x0 + df.x1)/2 < middle +30 )
+    ##large_font    = df.font_size >= median_font*1.15
+    ##centred       = ( (df.x0 + df.x1)/2 > middle -30 ) & ( (df.x0 + df.x1)/2 < middle +30 )
 
-    mask = bold_font & pages & uncategorised & after_headers & title_on_page
+    #mask = bold_font & pages & uncategorised & after_headers & title_on_page
     
-    result = df.loc[mask , ["text","font_size","font_sizes", "page","mode_font"]].copy()
+    #result = df.loc[mask , ["text","font_size","font_sizes", "page","mode_font"]].copy()
     #result = df.loc[df.title==1, ["text","font_size","font_sizes", "page","mode_font","common_font"] ]
+    result = df[df.subtitle==1].copy()
     result.loc[:,"font_multip"] = result.font_size/median_font
 
 
@@ -65,11 +67,6 @@ for year in range(2001,2026):
     pd.set_option('display.max_colwidth', 180)  # or a large number like 500
     print(result.text)
     print("--"*40)
-
-    #print(f"if year=={year}:")
-    #for i, row in result.iterrows():
-    #    print(f"    assert doc_df.loc[{i}].text=='{row.text}'")
-    #print("--"*40)
 
     #if year==2006:
     #    import ipdb; ipdb.set_trace()
