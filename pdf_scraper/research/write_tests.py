8f6bfc8ae2530c0e69eac499e9d9f7d85a184786
Daniel Tanner <danielsptanner@gmail.com>
2025-08-27 16:23:18 +0200

    beginning reformulatio of testing to use stored .txt files for golden master

--- pdf_scraper/research/write_tests.py
+++ pdf_scraper/research/write_tests.py
@@ -8,30 +8,44 @@ import sys, re
 from pdf_scraper.block_utils import identify_dual_column, get_block_text, sort_dual_column_blocks
 from pdf_scraper.doc_utils   import open_exam, get_doc_line_df, identify_section_headers, identify_text_headers
 from pdf_scraper.doc_utils   import identify_footers, identify_instructions, identify_subtitles, identify_subsubtitles
-from pdf_scraper.block_utils import is_empty_block, clean_blocks, print_block_table, get_block_table, rebox_blocks
-from pdf_scraper.block_utils import preproc_blocks
+from pdf_scraper.doc_utils   import get_images,filter_images, assign_in_image_captions
 from pdf_scraper.draw_utils  import get_pink_boundary, get_fill_df, in_the_pink
 from pdf_scraper.line_utils  import get_line_df, print_line_table, get_all_lines, line_is_empty, clean_line_df
 
+# choose category
+test_categories = ["dual_col", "caption","instruction", "footer", "section","title","subtitle","subsubtitle"]
+cat = "subtitle"
+
+OUT_DIR = Path(f"tests/expected_{cat}s")
+OUT_DIR.mkdir(parents=True, exist_ok=True)
 
 for year in range(2001,2026):
+
 
     doc = open_exam(year, "english", "al",1)
     df = get_doc_line_df(doc)
     doc_width     = doc[0].rect.width
+
+    images = get_images(doc)
+    images = filter_images(images)
+    assign_in_image_captions(df,images)
     
     df = clean_line_df(df)
+    
     identify_footers(df)
     identify_instructions(df)
     identify_section_headers(df)
     identify_text_headers(df, doc_width)
     identify_subtitles(df, doc_width)
-    identify_subsubtitles(df)
+    identify_subsubtitles(df, doc_width)
 
-    test_categories = ["dual_col", "caption","instruction", "footer", "section","title","subtitle","subsubtitle"]
-    cat = "subsubtitle"
     
     test_df = df[df[cat] == 1]
+
+    out_file = OUT_DIR / f"{year}.txt"
+    with out_file.open("w", encoding="utf-8") as f:
+        for text in test_df.text.tolist():
+            f.write(text + "\n")  # preserve \xa0 and all Unicode chars
     
     if year==2001:
         print(f"if year=={year}:")
