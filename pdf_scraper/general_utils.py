889cae754c8554314fd348b899be884c525c5223
Daniel Tanner <danielsptanner@gmail.com>
2025-09-07 11:43:23 +0200

    df_bbox_dist functions added

--- pdf_scraper/general_utils.py
+++ pdf_scraper/general_utils.py
@@ -48,3 +48,30 @@ def bbox_horiz_dist(bbox1, bbox2) -> float:
     if max(a1, a2) <= min(b1, b2):
         return 0.0
     return min(abs(b1 - a2), abs(b2 - a1))
+
+def df_bbox_next_row_dist(y0, y1, y0_next, y1_next):
+    """End-to-end vertical distance between two line segments.
+
+    This function will return 0 if the two bboxes overlap in the chosen dimension.
+    Otherwise it will return the distance between their closest endpoints.
+    """
+    overlap = np.maximum(y0, y0_next) <= np.minimum(y1, y1_next)
+    dist = np.where(overlap, 0.0, np.minimum(np.abs(y1 - y0_next), np.abs(y1_next - y0)))
+    return dist
+
+def df_bbox_dist(row1, row2):
+    """
+    This calculates the bbox end to end distance between to dataframe rows.
+
+    It can be used to generate a distance matrix between all rows of a dataframe of lines.
+    
+    df_bbox_dist(row1[["y0","y1]], row2[["y0","y1"]]) = vertical   end to end bbox distance
+    df_bbox_dist(row1[["x0","x1]], row2[["x0","x1"]]) = horizontal end to end bbox distance
+    """
+    y0, y1 = row1
+    y0_next, y1_next = row2
+
+    overlap = max(y0, y0_next) <= min(y1, y1_next)
+    if overlap:
+        return 0.0
+    return min(abs(y1 - y0_next), abs(y1_next - y0))
