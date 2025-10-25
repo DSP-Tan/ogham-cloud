9bfa88079c0632df5cc666b066da165e0d55879e
Daniel Tanner <danielsptanner@gmail.com>
2025-10-25 13:04:36 +0100

    command line argumnets added. To Do: add make file auto test resource creation command

--- pdf_scraper/tests/write_tests.py
+++ pdf_scraper/tests/write_tests.py
@@ -5,23 +5,40 @@ import fitz
 from fitz import Rect
 import sys, re
 
-from pdf_scraper.doc_utils   import (open_exam, get_doc_line_df, identify_section_headers, 
-                                     identify_text_headers,identify_footers, identify_instructions, 
-                                     identify_subtitles, identify_subsubtitles,get_images,preproc_images, 
+from pdf_scraper.doc_utils   import (open_exam, get_doc_line_df, identify_section_headers,
+                                     identify_text_headers,identify_footers, identify_instructions,
+                                     identify_subtitles, identify_subsubtitles,get_images,preproc_images,
                                      assign_in_image_captions, identify_vertical_captions,
                                      identify_all_page_clusters, enrich_doc_df_with_images)
 
 from pdf_scraper.line_utils  import clean_line_df
 
 
+
+test_categories = ["dual_col", "caption1","caption2", "instruction", "footer", "section","title","subtitle","subsubtitle"]
+n_args = len(sys.argv) -1
+if n_args < 1:
+    print(f"Usage: {sys.argv[0]} <option> <write_flag>")
+    print(f"Valid options: {', '.join(test_categories)}")
+    print("write_flag == 1 will trigger write to reserouces folder, no write_flag, or value !=1 writes just to terminal.")
+    sys.exit(1)
+
+
+cat = sys.argv[1].lower()
+if cat not in test_categories:
+    print(f"Error: '{cat}' is not a valid category.")
+    print(f"Valid options: {', '.join(test_categories)}")
+    sys.exit(1)
+print(f"Category '{cat}' selected. Proceeding...")
+
+if n_args ==1:
+    write = False
+else:
+    write = True if int(sys.argv[2]) == 1 else False
+
 paper=1
 level="al"
 subject="english"
-
-
-test_categories = ["dual_col", "caption1","caption2", "instruction", "footer", "section","title","subtitle","subsubtitle"]
-cat = "caption2"
-write = False
 
 out_dir = Path(__file__).parent.resolve() / Path(f"resources/expected_{cat}s")
 out_dir.mkdir(parents=True, exist_ok=True)
@@ -35,7 +52,7 @@ for year in range(2001,2026):
     images = get_images(doc)
     images = preproc_images(images)
     assign_in_image_captions(df,images)
-    
+
     df = clean_line_df(df)
     df = enrich_doc_df_with_images(df,images)
     identify_all_page_clusters(df,2.0/3.0, 1.15, True)
@@ -52,7 +69,7 @@ for year in range(2001,2026):
         identify_vertical_captions(df, img)
     #new_vertical_captions(df, images)
 
-    
+
     test_df = df[df.category == cat]
 
     if write:
@@ -60,7 +77,7 @@ for year in range(2001,2026):
         with out_file.open("w", encoding="utf-8") as f:
             for text in test_df.text.tolist():
                 f.write(text + "\n")  # preserve \xa0 and all Unicode chars
-    
+
     if year==2001:
         print(f"if year=={year}:")
     else:
